3705ac2a979a4562ba93b478d01131b406c2a62b
Meow <648593697@qq.com>
2018-03-24 00:31:19 +0800

    0324“
    git add commit -m 0324
    
    git
    
    :f
    git
    git tag

--- app/src/main/java/me/meoow/todo/MainActivity.java
+++ app/src/main/java/me/meoow/todo/MainActivity.java
@@ -1,13 +1,118 @@
 package me.meoow.todo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import me.meoow.todo.adapter.ViewPagerAdapter;
+import me.meoow.todo.fragment.TabFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    ArrayList<TabFragment> fragments;
+
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        }
+
+        navView.setCheckedItem(R.id.nav_about);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return true;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Data restored",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
+        initView();
+
+    }
+
+    private void initView() {
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabs);
+
+
+        //初始化数据
+        fragments = new ArrayList<>();
+        for (int i=0;i<3;i++){
+            fragments.add(new TabFragment("标题"+i,"内容"+i));
+        }
+        //设置ViewPager的适配器
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(),fragments);
+        viewPager.setAdapter(adapter);
+        //关联ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+        //设置固定的
+        //tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        //设置滑动的。
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            break;
+            default:
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/me/meoow/todo/adapter/ViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package me.meoow.todo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+import java.util.ArrayList;
+
+
+import me.meoow.todo.fragment.TabFragment;
+
+/**
+ * Created by Meow on 2018/3/23.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<TabFragment> fragments;
+    public ViewPagerAdapter(FragmentManager fm, ArrayList<TabFragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    /**
+     * 根据位置返回对应的Fragment
+     * @param position
+     * @return
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    /**
+     * 得到页面的标题
+     * @param position
+     * @return
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragments.get(position).getTitle();
+    }
+}
